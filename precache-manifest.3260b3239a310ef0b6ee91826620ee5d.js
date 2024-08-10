845a822bf89e62b0e44897193df294e04b4b2194
Piyush Sharma <111640586+Piyushsharma2004@users.noreply.github.com>
2024-08-10 08:21:35 +0530

    first commit

---
+++ precache-manifest.3260b3239a310ef0b6ee91826620ee5d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75278a318edcbb52b5be91b08ae1a46d",
+    "url": "/background.html"
+  },
+  {
+    "revision": "891ddc636d103011cf7837b7f61ab0d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab50038729b25d3deb2a246dbbb152a6",
+    "url": "/static/8ede052640bd2bf04ede.worker.js"
+  },
+  {
+    "revision": "e0b692b60aadfd3e116a",
+    "url": "/static/css/0.636c4284.chunk.css"
+  },
+  {
+    "revision": "e0b692b60aadfd3e116a",
+    "url": "/static/js/0.f06d9163.chunk.js"
+  },
+  {
+    "revision": "e75b7eb80be899522b912fe40af09459",
+    "url": "/static/js/0.f06d9163.chunk.js.LICENSE"
+  },
+  {
+    "revision": "841c0232cea59f81de7d",
+    "url": "/static/js/5.452e7431.chunk.js"
+  },
+  {
+    "revision": "bb6d7d4c4942fa5067f4894b57a9f4b4",
+    "url": "/static/js/5.452e7431.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05c31920538bcfdd3d05",
+    "url": "/static/js/background.87a81219.chunk.js"
+  },
+  {
+    "revision": "b0c8e7cfee8b3f5f9328",
+    "url": "/static/js/index.a9cd6040.chunk.js"
+  },
+  {
+    "revision": "f95baf81780a601d380c",
+    "url": "/static/js/runtime-background.c55605fb.js"
+  },
+  {
+    "revision": "81bac065a4d92ec8c6d1",
+    "url": "/static/js/runtime-index.ee9c4f9c.js"
+  }
+]);
